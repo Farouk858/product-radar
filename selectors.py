e173d015f79dec3d94c7958df919a866f8483fbf
Farouk858 <faroukalaofa@gmail.com>
2025-11-12 23:15:15 +0000

    Add generic keywords and brand hints to selectors

--- selectors.py
+++ selectors.py
@@ -1,1 +1,32 @@
+# selectors.py
+
+GENERIC_KEYWORDS = [
+    "bestseller", "best seller", "best-selling",
+    "back in stock", "restock", "restocked",
+    "most popular", "popular",
+    "trending",
+    "new arrivals", "new in", "just dropped",
+]
+
+# Optional brand-specific paths that often surface signals
+BRAND_HINTS = {
+    "Corteiz": {
+        "alts": [],
+    },
+    "Represent": {
+        "alts": ["/collections/new-arrivals", "/collections/bestsellers"],
+    },
+    "Supreme": {
+        "alts": ["/shop/new", "/shop/all"],
+    },
+    "Palace": {
+        "alts": ["/collections/new", "/collections/all"],
+    },
+    "Aimé Leon Dore": {
+        "alts": ["/collections/new-arrivals", "/collections/menswear"],
+    },
+    "Jaded London": {
+        "alts": ["/collections/new-in", "/collections/bestsellers"],
+    },
+}
 
